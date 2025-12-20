6f924345629613757f25647a38814231af4d1185
Ivan Iasnig <ivan.iasnig@emeal.nttdata.com>
2025-12-20 01:24:22 +0100

    feat: clean setup expo

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,65 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+
+const react = require("eslint-plugin-react");
+const prettier = require("eslint-plugin-prettier");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 module.exports = defineConfig([
-  expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["**/node_modules/**", "eslint.config.js"],
+    extends: compat.extends(
+      "expo",
+      "prettier",
+      "plugin:react/recommended",
+      "plugin:prettier/recommended"
+    ),
+
+    plugins: {
+      react,
+      prettier,
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+    },
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "prettier/prettier": "error",
+      "react/jsx-curly-brace-presence": "error",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/preserve-manual-memoization": "off",
+      "react-hooks/immutability": "off",
+      "react-hooks/refs": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/use-memo": "off",
+      "react-hooks/static-components": "off",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/no-unknown-property": "off",
+      "react/jsx-key": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
   },
 ]);
